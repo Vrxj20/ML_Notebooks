f42c2ead5dff13a729beb8297776e3c12f7cfe76
Vraj Patel <vrajudi7807@gmail.com>
2025-08-18 13:16:21 +0530

    Added intro of flask lib

---
+++ Flask/Intro_flask.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to my first ever flask page"
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
